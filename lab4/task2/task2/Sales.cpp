88a35e0d2cd3cbbe9c38cefdcb19a3e9590bad37
middle <example@example.com>
2022-10-14 01:57:03 +0300

    Orphographical chanes

--- lab4/task2/task2/Sales.cpp
+++ lab4/task2/task2/Sales.cpp
@@ -7,12 +7,12 @@ Sales::Sales() {
 }
 void Sales::setData() {
 	for (int i = 0; i < 3; i++) {
-		std::cout << "Enter sales for " << i << " mounth: ";
+		std::cout << "Enter sales for " << i + 1 << " mounth: ";
 		std::cin >> this->totalPrice[i];
 	}
 }
 void Sales::getData() const {
 	for (int i = 0; i < 3; i++) {
-		std::cout << "Sales for " << i << " mounth are: " << this->totalPrice[i] << std::endl;
+		std::cout << "Sales for " << i + 1 << " mounth are: " << this->totalPrice[i] << std::endl;
 	}
 }
