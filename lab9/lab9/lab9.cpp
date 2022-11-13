e3eb4b3af2366ce347e7852f50cd3a9665a6b925
middle <example@example.com>
2022-11-13 22:24:46 +0200

    init commit

---
+++ lab9/lab9/lab9.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream> // Для потокового введення-виведення
+#include <cstring> // Для роботи з рядковими типами даних
+#include "windows.h"
+
+using namespace std;
+#include <process.h> //for exit()
+const int LIMIT = 100; //array size
+////////////////////////////////////////////////////////////////
+class safearay
+{
+private:
+	int arr[LIMIT];
+public:
+	int& operator [](int n)
+	{
+		if (n < 0 || n >= LIMIT)
+		{
+			cout << "\nIndex out of bounds"; exit(1);
+		}
+		return arr[n];
+	}
+};
+////////////////////////////////////////////////////////////////
+int main()
+{
+	safearay sa1;
+	int j;
+	for (int j = 0; j < LIMIT; j++)
+		sa1[j] = j * 10; //*left* side of equal sign
+	for (j = 0; j < LIMIT; j++) //display elements
+	{
+		int temp = sa1[j]; //*right* side of equal sign
+		cout << "Element " << j << " is " << temp << endl;
+	}
+	return 0;
+}
